ce812c38ef72559894526defbc2da088d7664e92
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-12 19:30:36 +0530

    feat: switch hero to realistic Indian home scene

--- components/3d/HeroScene3D.tsx
+++ components/3d/HeroScene3D.tsx
@@ -1,3 +1,3 @@
 "use client";
-export { DetailedHeroScene3D as HeroScene3D } from "./DetailedHeroScene3D";
-export type { } from "./DetailedHeroScene3D";
+export { RealisticHeroScene3D as HeroScene3D } from "./RealisticHeroScene3D";
+export type { } from "./RealisticHeroScene3D";
